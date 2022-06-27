63eff4dd61ce1600f091165926f75934c9332648
Leonard Baranski | Numbat GmbH <leonard.baranski@numbat.energy>
2022-06-27 13:45:36 +0200

    Linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,7 +24,7 @@ class ModelTests(TestCase):
         """Test email ist normalized for new users"""
         sample_emails = [
             ["test1@EXAMPLE.com", "test1@example.com"],
-            ["Test2@Example.com","Test2@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
             ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
             ["test4@example.COM", "test4@example.com"],
         ]
